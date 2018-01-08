06f7917e2ef47b54ff0fff737fdab96034724728
Jan Šubelj <jan.subelj010@gmail.com>
2018-01-08 20:14:31 +0100

    init

---
+++ src/main/java/com/cleptes/crypto/App.java
@@ -0,0 +1,13 @@
+package com.cleptes.crypto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
